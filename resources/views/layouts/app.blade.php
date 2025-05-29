0e604b0e460c430672b9334ff01ec39222397ab5
Ishan Sheikh <sheikh.ysbh@gmail.com>
2025-05-29 16:53:19 +0530

    Added fonts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,12 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Funnel+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+
     <title inertia>{{ config('app.name', 'unfiltered.') }}</title>
 
     @vite(['resources/css/app.css'])
